d62432cb4ab87ebe92a97acd6d63d33633679872
annatorok <mail@annatorok.com>
2016-05-23 09:50:18 +0200

    Add exercise 10

--- week-03/day-3/10.py
+++ week-03/day-3/10.py
@@ -7,3 +7,14 @@
 #  ***********
 #
 # It should take a number as parameter that describes how many lines the triangle has
+
+def xmas_tree(input):
+    star = 1
+    space = input
+    for row in range(1, input + 1):
+        print(' ' * space + '*' * star)
+        star += 2
+        space -=1
+
+
+xmas_tree(8)

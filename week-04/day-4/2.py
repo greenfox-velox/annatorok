9b5d3890b9c61ce1bec863743fb42740bb912cb2
annatorok <mail@annatorok.com>
2016-05-26 23:58:40 +0200

    Modify day 4 exercise 2 and 11

--- week-04/day-4/2.py
+++ week-04/day-4/2.py
@@ -2,21 +2,21 @@
 # that takes one parameter: n
 # and adds numbers from 1 to n
 
-def summa(n):
-    if n <= 0:
-        return 0
-    else:
-        return summa(n-1) + n
-
-print(summa(4))
-
-
-
-# def concat(n):
+# def summa(n):
 #     if n <= 0:
-#         return []
+#         return 0
 #     else:
-#         return concat(n-1) + [n]
+#         return summa(n-1) + n
 #
-#
-# print(concat(6))
+# print(summa(4))
+
+
+
+def concat(n):
+    if n <= 0:
+        return []
+    else:
+        return concat(n-1) + [n]
+
+
+print(concat(6))

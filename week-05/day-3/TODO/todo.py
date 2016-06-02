bc1592345b1e68d7887ebb563e0f8a79456a2668
annatorok <mail@annatorok.com>
2016-06-02 08:41:54 +0200

    Added missing file handling and restructured the main function

--- week-05/day-3/TODO/todo.py
+++ week-05/day-3/TODO/todo.py
@@ -26,23 +26,31 @@ class Todo():
             print('Unsupported argument')
             self.main_menu()
 
+    def argv_is_a(self):
+        if len(sys.argv) == 2:
+            print('Unable to add: No task is provided')
+        else:
+            self.add_to_list(sys.argv[2])
+
+    def argv_is_r(self):
+        if len(sys.argv) == 2:
+            print('Unable to remove: No index is provided')
+        else:
+            self.remove_from_list(sys.argv[2])
+
+
     def main(self):
-        self.ok_argument()
+        self.missing_file()
         if len(sys.argv) == 1:
             self.main_menu()
         else:
+            self.ok_argument()
             if sys.argv[1] == '-l':
                 print(self.load_list())
             elif sys.argv[1] == '-a':
-                if len(sys.argv) == 2:
-                    print('Unable to add: No task is provided')
-                else:
-                    self.add_to_list(file_name, sys.argv[2])
+                self.argv_is_a()
             elif sys.argv[1] == '-r':
-                if len(sys.argv) == 2:
-                    print('Unable to remove: No index is provided')
-                else:
-                    self.remove_from_list(file_name, sys.argv[2])
+                self.argv_is_r()
 
     def load_list(self):
         try:
@@ -64,14 +72,16 @@ class Todo():
 
     def add_to_list(self, new_todo_element):
         f = open(self.file_name, 'a')
-        new_todo_list = f.write(new_todo_element + '\n')
+        f.write(new_todo_element + '\n')
         f.close()
 
     def remove_from_list(self, remove_n):
         f = open(self.file_name)
         remove_line = f.readlines()
         try:
-            if (len(remove_line)-1) < int(remove_n):
+            if (len(remove_line)) == 1 and remove_n == len(remove_line):
+                remove_line.remove(remove_line(0))
+            elif (len(remove_line)) < int(remove_n):
                 print('Unable to remove: Index is out of bound')
             else:
                 remove_line.remove(remove_line[int(remove_n)-1])
@@ -83,10 +93,14 @@ class Todo():
             f.write(i)
         f.close()
 
+    def create_file(self):
+        f = open(todo_list+'.'+txt, 'a')
+
     def missing_file(self):
-        f = open(self.file_name, 'w')
-        f.write('')
-        f.close()
+        try:
+            f = open(self.file_name, 'a')
+        except FileNotFoundError:
+            self.create_file()
 
 
 todo = Todo()
